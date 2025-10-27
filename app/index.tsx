aa729da6cc7e329c4726afeee1cdf0a35d3ae8bd
Kevin Christ <christkevin50@gmail.com>
2025-10-27 21:36:13 +0100

    updated index with list

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,44 @@
-import { Text, View } from "react-native";
+import { Text, StyleSheet } from "react-native";
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { FlashList } from "@shopify/flash-list";
+
+const DATA = [
+    {
+        title: "First Item",
+        id: "1"
+    },
+    {
+        title: "Second Item",
+        id: "2"
+    },
+]
+
+const MyList = () => {
+    return (
+        <FlashList
+            data={DATA}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (<Text style={styles.list}>{item.title}</Text>)}
+
+        />
+    );
+};
+
 
 export default function Index() {
     return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-            }}
-        >
-            <Text> Hello, this is Verin! </Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <MyList />
+        </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    list: {
+        padding: 15,
+        fontSize: 16,
+    }
+});
