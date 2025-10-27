5ad28654b41d9c5e4c83e5421846ba1b5e449faf
Kevin Christ <christkevin50@gmail.com>
2025-10-27 21:35:45 +0100

    changed from stack to tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,24 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import Feather from '@expo/vector-icons/Feather';
 
-export default function RootLayout() {
-  return <Stack />;
+export default function TabLayout() {
+    return (
+        <Tabs >
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: '',
+                    tabBarIcon: ({ color }) => <Feather name="home" size={24} color={color} />
+                }}
+            />
+
+            <Tabs.Screen
+                name="settings"
+                options={{
+                    title: '',
+                    tabBarIcon: ({ color }) => <Feather name="settings" size={24} color={color} />
+                }}
+            />
+        </Tabs>
+    )
 }
